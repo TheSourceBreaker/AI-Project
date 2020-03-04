7793467292c651a23ea5a1e69c1235fe63e79b3a
Jahvier <50281954+TheSourceBreaker@users.noreply.github.com>
2020-03-04 09:09:04 -0800

    Update

---
+++ Assets/Scripts/Assassin.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assassin : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject bush; //for each bush, go through an array and measure the distance between the player and the bush. the closest bush index gets saved then checked again with the player distance and to see if they are close together before choosing
+
+    public IDecision currentDecision;
+    IDecision AssassinAI;
+
+    private void Update()
+    {
+        AssassinAI = new 
+
+        currentDecision = AssassinAI;
+
+        if (currentDecision != null)
+        {
+            currentDecision = currentDecision.makeDecision();
+        }
+    }
+}
+
+public class DoISeePlayer
+{
+    bool value;
+    IDecision trueBranch;
+    IDecision falseBranch;
+
+    DoISeePlayer(IDecision trueBranch, IDecision falseBranch, Assassin assassin)
+    {
+        if (Vector3.Distance(assassin.enemy.transform.position, assassin.player.transform.position) < 1) // use radius/collider
+        {
+            value = true;
+            this.trueBranch = trueBranch;
+        }
+        else
+        {
+            value = false;
+            this.falseBranch = falseBranch;
+        }
+    }
+
+    IDecision makeDecision()
+    {
+        if(value == true)
+        {
+            return trueBranch;
+        }
+        else if(value == false)
+        {
+            return falseBranch;
+        }
+
+        return null;
+    }
+}
+
+public class IsPlayerInBush
+{
+    bool value;
+    IDecision trueBranch;
+    IDecision falseBranch;
+
+    IsPlayerInBush(IDecision trueBranch, IDecision falseBranch, Assassin assassin)
+    {
+        if(Vector3.Distance(assassin.player.transform.position, assassin.bush.transform.position) < 0.6)
+        {
+            value = true;
+            this.trueBranch = trueBranch;
+        }
+        else
+        {
+            value = false;
+            this.falseBranch = falseBranch;
+        }
+    }
+    IDecision makeDecision()
+    {
+        if (value == true)
+        {
+            return trueBranch;
+        }
+        else if (value == false)
+        {
+            return falseBranch;
+        }
+
+        return null;
+    }
+}
+
+public class AmICloseToPlayer
+{
+    bool value;
+    IDecision trueBranch;
+    IDecision falseBranch;
+
+    AmICloseToPlayer(IDecision trueBranch, IDecision falseBranch, Assassin assassin)
+    {
+        if (Vector3.Distance(assassin.player.transform.position, assassin.bush.transform.position) < 0.6)
+        {
+            value = true;
+            this.trueBranch = trueBranch;
+        }
+        else
+        {
+            value = false;
+            this.falseBranch = falseBranch;
+        }
+    }
+    IDecision makeDecision()
+    {
+        if (value == true)
+        {
+            return trueBranch;
+        }
+        else if (value == false)
+        {
+            return falseBranch;
+        }
+        return null;
+    }
+}
+
+public class WanderAround
+{
+    Assassin assassin;
+
+    WanderAround(Assassin ass
+        )
+    {
+
+    }
+}
+
+public class AttackPlayer
+{
+
+}
+
+public class ChasePlayer
+{
+
+}
---
+++ Assets/Scripts/Wanderer.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wanderer : MonoBehaviour
+{
+    public int health;
+    public float speed;
+
+    public GameObject player;
+    public GameObject enemy;
+    public GameObject bush;
+
+    public IDecision currentDecision;
+    IDecision WandererAI;
+
+    private void Update()
+    {
+        WandererAI = new DoISeeEnemy(new AmIInBush(new StayInBush(this), new DoISeeBush(new HideInBush(this), new Panic(this), this), this), new ContinuePath(this), this);
+
+        currentDecision = WandererAI;
+
+        if (currentDecision != null)
+        {
+            currentDecision = currentDecision.makeDecision();
+        }
+    }
+}
+
+public interface IDecision
+{
+    IDecision makeDecision();
+}
+
+public class DoISeeEnemy : IDecision
+{
+    bool value;
+    IDecision trueBranch;
+    IDecision falseBranch;
+    public DoISeeEnemy(IDecision trueBranch, IDecision falseBranch,Wanderer wanderer)
+    {
+        if (Vector3.Distance(wanderer.player.transform.position, wanderer.enemy.transform.position) < 1) // use radius/collider
+        {
+            value = true;
+            this.trueBranch = trueBranch;
+        }
+        else
+        {
+            value = false;
+            this.falseBranch = falseBranch;
+        }
+    }
+
+    public IDecision makeDecision()
+    {
+        if (value == true)
+        {
+            return trueBranch;
+        }
+        else if (value == false)
+        {
+            return falseBranch;
+        }
+        return null;
+    }
+}
+
+public class AmIInBush : IDecision
+{
+    bool value;
+    IDecision trueBranch;
+    IDecision falseBranch;
+    public AmIInBush(IDecision trueBranch, IDecision falseBranch, Wanderer wanderer) // use radius
+    {
+        if (Vector3.Distance(wanderer.player.transform.position, wanderer.bush.transform.position) < 1)
+        {
+
+            value = true;
+            this.trueBranch = trueBranch;
+        }
+        else
+        {
+            value = false;
+            this.falseBranch = falseBranch;
+
+        }
+    }
+
+    public IDecision makeDecision()
+    {
+        if (value == true)
+        {
+            return trueBranch;
+        }
+        else if (value == false)
+        {
+            return falseBranch;
+        }
+        return null;
+    }
+}
+
+public class DoISeeBush : IDecision
+{
+    bool value;
+    IDecision trueBranch;
+    IDecision falseBranch;
+    public DoISeeBush(IDecision trueBranch, IDecision falseBranch, Wanderer wanderer)
+    {
+        if (Vector3.Distance(wanderer.player.transform.position, wanderer.bush.transform.position) < 1 && wanderer.enemy.CompareTag("enemy")) // use radius
+        {
+            value = true;
+            this.trueBranch = trueBranch;
+        }
+        else
+        {
+            value = false;
+            this.falseBranch = falseBranch;
+        }
+    
+    }
+    public IDecision makeDecision()
+    {
+        if (value == true)
+        {
+            return trueBranch;
+        }
+        else if (value == false)
+        {
+            return falseBranch;
+        }
+        return null;
+    }
+}
+
+public class ContinuePath : IDecision
+{
+    Wanderer wanderer;
+    public ContinuePath(Wanderer _wanderer)
+    {
+        wanderer = _wanderer;
+    }
+
+    public IDecision makeDecision()
+    {
+        // continue Pathfinding
+        return null;
+    }
+}
+
+public class StayInBush : IDecision
+{
+    Wanderer wanderer;
+    public StayInBush(Wanderer _wanderer)
+    {
+        wanderer = _wanderer;
+    }
+
+    public IDecision makeDecision()
+    {
+        // Hold position
+        return null;
+    }
+}
+
+
+public class Panic : IDecision
+{
+    Wanderer wanderer;
+    public Panic(Wanderer _wanderer)
+    {
+        wanderer = _wanderer;
+    }
+
+    public IDecision makeDecision()
+    {
+        //Run around the place
+        return null;
+    }
+}
+
+
+public class HideInBush : IDecision
+{
+    Wanderer wanderer;
+    public HideInBush(Wanderer _wanderer)
+    {
+        wanderer = _wanderer;
+    }
+
+    public IDecision makeDecision()
+    {
+        //Run to Bush
+        return null;
+    }
+}
+
+
